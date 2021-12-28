6386f0c39f1877da39de358f3deb83bcd9cf0980
catiemo <caleb.keseku@amalitech.org>
2021-12-28 04:58:30 +0000

    Create Whale Talk.js

---
+++ Whale Talk.js
@@ -0,0 +1,20 @@
+let input = 'a whale of a deal!';
+
+const vowels = ['a', 'i', 'e', 'o', 'u'];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  // console.log(i);
+  for (let j = 0; j < vowels.length; j++) {
+    // console.log(j);
+    if (input[i] === vowels[j]) {
+      resultArray.push(vowels[j]);
+    if (input[i] === 'e' || input[i] === 'u') {
+      resultArray.push(input[i]);
+    }
+    }
+  }
+}
+
+console.log(resultArray.join('').toUpperCase());
